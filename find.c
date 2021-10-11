0ede316625adf0097743aa193138134755ebcdf6
Sujit S. Molleti <ssm189@ilab1.cs.rutgers.edu>
2021-10-11 01:57:41 -0400

    done with find

--- find.c
+++ find.c
@@ -5,13 +5,10 @@
 #include <errno.h>
 #include <string.h>
 
-void find(char* searchStr, char* directory, char* path){
-
+void find(char* searchStr, char* path){
 	// List the contents of the directory -- readdir? Or should we use ls and pipe?
-
-	
 	struct dirent* dir;
-	DIR* dirp= opendir(directory);
+	DIR* dirp= opendir(path);
 	errno = 0;
 
 	// Iterate through the contents
@@ -28,24 +25,29 @@ void find(char* searchStr, char* directory, char* path){
 			if(strcmp(dir->d_name, ".git") == 0){
 				continue;
 			}
+			if(strcmp(dir->d_name, ".gitignore") == 0){
+				continue;
+			}
+			
+			// +2: one for the extra slash, and then one for the null-terminating
+			char* newPath = malloc((strlen(path) + strlen(dir->d_name) + 2) * sizeof(char));
+			strcpy(newPath, path);
+			strcat(newPath, dir->d_name);
+			strcat(newPath, "/");
 
-			char* newPath = malloc((strlen(path) + strlen(dir->d_name)) * sizeof(char));
-			newPath = strcat(newPath, path);
-			newPath = strcat(newPath, dir->d_name);
-			newPath = strcat(newPath, "/");
-
-			find(searchStr, dir->d_name, newPath);
+			find(searchStr, newPath);
 
 			free(newPath);
+
 		} else {
 			if(strstr(dir->d_name, searchStr)){
 				printf("%s%s\n", path, dir->d_name);
 			}
 		}
+
 	}
 
 	closedir(dirp);
-
 	if(errno)
 		printf("error!");
 }
@@ -54,9 +56,7 @@ int main(int argc, char **argv){
 
 	// Get Substring argv[0]
 	char* searchStr = argv[1];
-
-	//get stats on current dirrectory
-	find(searchStr, ".", "./");
+	find(searchStr, "./");
 
 	return 0;
 
